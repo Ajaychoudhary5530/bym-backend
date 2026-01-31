a2141875a0c9ed2cc31f53eb168c46471c965691
Ajay <ajaybym@gmail.com>
2026-01-31 16:18:11 +0530

    role fixed

--- routes/productBulkRoutes.js
+++ routes/productBulkRoutes.js
@@ -2,15 +2,18 @@ import express from "express";
 import multer from "multer";
 import { bulkUploadProducts } from "../controllers/productBulkController.js";
 import { protect } from "../middleware/authMiddleware.js";
-import { adminOnly } from "../middleware/roleMiddleware.js";
+import { superAdminOnly } from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
 
+/* =========================
+   BULK UPLOAD (SUPER ADMIN ONLY)
+========================= */
 router.post(
   "/bulk-upload",
   protect,
-  adminOnly,
+  superAdminOnly,
   upload.single("file"),
   bulkUploadProducts
 );
