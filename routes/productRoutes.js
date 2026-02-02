3886215dbc82be5a7a699e6abdb064f1a5df7807
Ajay <ajaybym@gmail.com>
2026-02-02 10:46:22 +0530

    single upload fixed

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,16 +1,14 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { adminOnly } from "../middleware/roleMiddleware.js";
-
-import {
-  getProductsWithStock,
-} from "../controllers/productController.js";
+import { createProduct, getProductsWithStock } from "../controllers/productController.js";
 
 const router = express.Router();
 
-/* =========================
-   DASHBOARD
-========================= */
+// CREATE SINGLE PRODUCT
+router.post("/", protect, adminOnly, createProduct);
+
+// DASHBOARD
 router.get("/with-stock", protect, getProductsWithStock);
 
 export default router;
