a7bce111ce2f51e07791eca95c1626c8afdb1d1d
Ajay <ajaybym@gmail.com>
2026-02-02 10:48:55 +0530

    deafualt

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,16 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { adminOnly } from "../middleware/roleMiddleware.js";
-import { createProduct, getProductsWithStock } from "../controllers/productController.js";
+
+import {
+  getProductsWithStock,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-// CREATE SINGLE PRODUCT
-router.post("/", protect, adminOnly, createProduct);
-
-// DASHBOARD
+/* =========================
+   DASHBOARD
+========================= */
 router.get("/with-stock", protect, getProductsWithStock);
 
 export default router;
